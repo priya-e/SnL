021b7ead5d48f4e7bc2ff3f8d176604ee5389e40
Priyanka Maran <30787455+priya-e@users.noreply.github.com>
2017-08-20 21:28:12 -0700

    Add files via upload

--- snake_and_ladder.py
+++ snake_and_ladder.py
@@ -7,20 +7,25 @@ game_log = []
 
 class Player(object):
     def __init__(self,initial,position):
-        self.initial = initial
+        self.initial = initial.upper()
         self.position = position
+        self.last_number = 0
     
     def play(self):
-        print "Player",self.initial, 'press enter to roll the dice'
+        print "Player",self.initial.upper(), 'press enter to roll the dice'
         raw_input()
         number = dice_roll()
+        self.last_number = number
+        position_log = self.initial + " Current position: " + str(self.position) + ", Dice roll: " + str(self.last_number)
+        game_log.append(position_log)
         if number + self.position > 100:
             print 'Invalid Dice roll!'
         else:
             self.position = number + self.position
         self.follow_snake_and_ladder()
-        position_log = self.initial + " position " + str(self.position)
-        game_log.append(position_log)
+        moved = self.initial + " Moved to "+ str(self.position)+ " position"
+        game_log.append(moved)
+        
         
     def is_winner(self):
         if self.position == 100:
@@ -31,11 +36,11 @@ class Player(object):
     def follow_snake_and_ladder(self):
         while self.position in snake or self.position in ladder:
             if self.position in snake:
-                position_log = 'Snake at '+ str(self.position) +' snake to ' + str(snake[self.position])
+                position_log = 'Oops, snake at '+ str(self.position) +'. ' + self.initial + ' falls down to ' + str(snake[self.position]) + ' position'
                 self.position = snake[self.position]
                 game_log.append(position_log)
             else:
-                position_log = 'Ladder at '+ str(self.position) +' Ladder to ' + str(ladder[self.position])
+                position_log = 'Hurrey, ladder at '+ str(self.position) +'. ' + self.initial +' climbs up to ' + str(ladder[self.position])+' position'
                 self.position = ladder[self.position]
                 game_log.append(position_log)
 
@@ -67,8 +72,28 @@ ladder = {
     78:100
 }
 
-player1 = Player('A',0)
-player2 = Player('B',0)
+while True:
+    p1 = raw_input('Player 1: Please enter your name(less than 10 characters): ')
+    if len(p1) <= 10: break
+    else:
+        print 'Name must be less than 10 characters'
+        continue
+# Assigning Intial to player2
+while True:
+    p2 = raw_input('Player 2: Please enter your name(less than 10 characters): ')
+    if len(p2) <= 10: 
+        if p2 != p1: 
+            break
+        else:
+            print 'Name already chosen by Player1. Please enter adifferent name: '
+            continue
+    else:
+        print 'Name must be less than 10 characters: '
+        continue
+
+
+player1 = Player(p1,0)
+player2 = Player(p2,0)
 current_player = [player1]
 
 def print_board_row(row):
@@ -76,22 +101,32 @@ def print_board_row(row):
     # if row  number is odd, j takes values 9 to 0
     if row%2==1:
         for col in range(9,-1,-1):
-             print '%12s' % matrix[10*row+col],'|', #
+            colored_number = colored(str(matrix[10*row+col]),'white')
+            colored_number = ' '* (12 - len(str(matrix[10*row+col]))) + colored_number
+            print colored_number,'|', #
     # if row number is even then j takes values from 0 to 9
     else:
         for col in range(10):
-                 print '%12s' % matrix[10*row+col],'|',
+            colored_number = colored(str(matrix[10*row+col]),'white')
+            colored_number = ' '* (12 - len(str(matrix[10*row+col]))) + colored_number
+            print colored_number,'|',
     print ''
 
 def get_player_cell(row, col):
     index = matrix[10*row+col]
     a = '|'
+    spaces = 14
     player_cell_output = ''
     if player1.position == index:
-        player_cell_output = player_cell_output + player1.initial
+        player_cell_output = player_cell_output + colored(player1.initial,'yellow',attrs=['bold','underline','blink'])
+        spaces = spaces - len(player1.initial)
     if player2.position == index:
-        player_cell_output = player_cell_output + player2.initial
-    return a + player_cell_output.center(14, ' ')
+        player_cell_output = player_cell_output + colored(player2.initial,'cyan',attrs=['bold','underline','blink'])
+        spaces = spaces - len(player2.initial)
+    #spaces to fill
+    left_spaces = spaces/2
+    right_spaces = spaces - left_spaces
+    return a + ' '*left_spaces + player_cell_output + ' '*right_spaces
 
 def print_player_row(row):
     output =''
@@ -160,15 +195,23 @@ def flip():
         current_player[0] = player1
 
 def game():
+    print_board()
     while True:
-        print_board()
-        log_last_5_moves()
         current_player[0].play()
         print_board()
+        #log_last_5_moves()
+
         if current_player[0].is_winner():
             print 'Player',current_player[0].initial,'has won the game'
             break
-        flip()
+        if current_player[0].last_number != 6:
+            game_log.append('-------------------------------------------------------')
+            flip()
+        else:
+            game_log.append('Player ' + current_player[0].initial+ ', since you got 6, it is your turn again')
+        log_last_5_moves()
+
+       
 
 
 game()
